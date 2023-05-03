8dee2eafa1895b82831d08dab43d470d7db372c6
Petr Hanzl <petrhanzl33@gmail.com>
2023-05-03 22:55:43 +0200

    Add changes

--- pages/result.vue
+++ pages/result.vue
@@ -1,14 +1,19 @@
 <template>
-    <v-container class="d-flex justify-center">
-        <v-sheet class="mb-10 pt-10 rounded-xl " elevation="10">
-
-            <img src="static/result-graph.png" />
-        </v-sheet>
-    </v-container>
+    <v-row class="d-flex justify-center">
+        <v-col cols="8">
+            <p class="text-h4 text-center my-6 text-primary">Výsledky</p>
+            <v-sheet elevation="4" class="pa-4 pa-md-10 ma-4 ma-md-16" rounded="xl">
+                <v-img :src="picChart" />
+            </v-sheet>
+        </v-col>
+    </v-row>
 </template>
 
 <script setup lang="ts">
-
+const picChart = new URL(
+    "../static/result-graph.png",
+    import.meta.url
+).href;
 </script>
 <style scoped>
 
