b4548f4456dba9c9be32d4774f9cb82156565095
Petr Hanzl <petrhanzl33@gmail.com>
2023-05-06 11:26:26 +0200

    Modify styling in questionnaire.vue; Add submit button

--- pages/questionnaire.vue
+++ pages/questionnaire.vue
@@ -1,14 +1,32 @@
 <template>
     <v-container class="d-flex justify-center">
         <v-sheet class="pa-4 pa-sm-7 rounded-xl d-flex justify-center align-center" elevation="5">
-            <div v-for="q in store.getAllQuestions">
-                <div v-if="q.answerCategory.value==='Scale'">
-                    <QuestionSlider :text="q.questionText"/>
-                </div>
-                <div v-else>
-                    <QuestionYesNo :text="q.questionText"/>
-                </div>
-            </div>
+
+                <v-col class="d-block">
+                    <div v-for="q in store.getAllQuestions">
+                        <div v-if="q.answerCategory.value==='Scale'">
+                            <QuestionSlider :question="q"/>
+                        </div>
+                        <div v-else>
+                            <QuestionYesNo :text="q.questionText"/>
+                        </div>
+
+                    </div>
+                    <nuxt-link :to="{name:'result'}">
+                        <v-btn
+                            class="ma-4"
+                            type="submit"
+                            rounded
+                            color="primary"
+                            variant="tonal"
+                            size="x-large"
+                        >Odeslat</v-btn>
+                    </nuxt-link>
+                </v-col>
+
+
+
+
         </v-sheet>
     </v-container>
 </template>
