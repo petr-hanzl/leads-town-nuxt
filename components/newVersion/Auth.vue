3842d6f5851ae2548ad4b25487ba34ef555b1733
Dita Němečková <ditusn@gmail.com>
2023-05-06 21:16:19 +0200

    fix design auth

---
+++ components/newVersion/Auth.vue
@@ -0,0 +1,179 @@
+<!-- TODO toto si pak hod do indexu, bud zkopiruj kod a smaz Auth.vue nebo to vloz jako komponentu-->
+<template>
+    <v-container fluid class="fill-height h-screen d-flex flex-column justify-space-between bg-white pa-0">
+        <v-row class="w-100 h-100">
+            <v-col cols="12" lg="4" class="px-3 pb-0">
+                <!-- back button -->
+                <v-row class="w-100 pa-6">
+                    <v-col class="d-flex justify-start">
+                        <v-btn icon="mdi-keyboard-backspace" color="primary-lighten-2"></v-btn>
+                    </v-col>
+                    <!-- logo TODO nuxt images -->
+                    <v-col class="d-flex justify-end" v-if="mdAndDown">
+                        <img src="assets/images/logo.png"  alt="logo leads town"/>
+                    </v-col>
+                </v-row>
+                <!-- text -->
+                <v-row class="w-100 pa-6 mt-0 d-flex flex-column align-content-lg-space-between text-primary">
+                    <v-col>
+                        <p class="text-h4 text-sm-h3 font-weight-black">
+                            Test
+                        </p>
+                    </v-col>
+                    <v-col class="pt-lg-10">
+                        <p class="text-h5 text-sm-h4 font-weight-bold">
+                            Využíváte marketing naplno?
+                        </p>
+                    </v-col>
+                    <v-col>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur. Tortor blandit enim habitant fermentum porta pharetra
+                            porta egestas sit. Purus in accumsan leo. Lorem ipsum dolor sit amet consectetur. Tortor blandit enim habitant fermentum porta pharetra
+                            porta egestas sit. Purus in accumsan leo. Lorem ipsum dolor sit amet consectetur. Tortor blandit enim habitant fermentum porta pharetra
+                            porta egestas sit. Purus in accumsan leo.
+                        </p>
+                    </v-col>
+                </v-row>
+            </v-col>
+
+            <v-col cols="12" lg="8" class="px-0 pb-0">
+                <v-sheet color="primary" class="h-100 w-100 rounded-corner d-flex align-center">
+                    <v-container fluid class="fill-height d-flex flex-column justify-space-between px-6 pb-9">
+
+                        <v-row class="w-100 pt-2" v-if="!mdAndDown">
+                            <!-- logo TODO nuxt images -->
+                            <v-col class="d-flex justify-end">
+                                <img src="assets/images/logo.png"  alt="logo leads town"/>
+                            </v-col>
+                        </v-row>
+
+                        <v-row justify="center" class="w-100 pe-lg-16 align-center mt-10 mt-lg-0">
+                            <form @submit.prevent="submit" class="pe-lg-16 me-lg-16">
+                                <v-text-field
+                                    rounded
+                                    variant="solo"
+                                    base-color="primary"
+                                    color="primary"
+                                    v-model="firstname.value.value"
+                                    :error-messages="firstname.errorMessage.value!"
+                                    label="Jméno"
+                                    class=""
+                                ></v-text-field>
+
+                                <v-text-field
+                                    rounded
+                                    variant="solo"
+                                    base-color="primary"
+                                    color="primary"
+                                    v-model="lastname.value.value"
+                                    :error-messages="lastname.errorMessage.value!"
+                                    label="Příjmení"
+                                ></v-text-field>
+
+                                <v-text-field
+                                    rounded
+                                    variant="solo"
+                                    base-color="primary"
+                                    color="primary"
+                                    v-model="email.value.value"
+                                    :error-messages="email.errorMessage.value!"
+                                    label="E-mail"
+                                ></v-text-field>
+
+                                <v-checkbox
+                                    v-model="checkbox.value.value"
+                                    :error-messages="checkbox.errorMessage.value!"
+                                    value="1"
+                                    label="Přijímám zasílání marketingových emailů"
+                                    type="checkbox"
+                                ></v-checkbox>
+
+                                <v-row class="d-flex justify-end">
+                                    <!--<NuxtLink :to="{name:'form'}">-->
+                                    <v-btn
+                                        class="me-4 text-body-1 text-primary font-weight-bold"
+                                        type="submit"
+                                        rounded
+                                        color="secondary"
+                                        size="large"
+                                    >
+                                        Odeslat
+                                    </v-btn>
+                                    <!-- </NuxtLink>-->
+                                </v-row>
+                            </form>
+                        </v-row>
+
+                        <v-row class="w-100 text-primary-lighten-2 text-h6">
+                            <v-col class="d-flex justify-end align-end">
+                                <p>02</p>
+                            </v-col>
+                        </v-row>
+
+                    </v-container>
+                </v-sheet>
+            </v-col>
+
+        </v-row>
+    </v-container>
+</template>
+
+<script setup lang="ts">
+
+
+import {useField, useForm} from 'vee-validate'
+import {useUserStore} from "~/store/users";
+import { useDisplay } from "vuetify";
+
+const { mdAndDown } = useDisplay();
+
+const {handleSubmit} = useForm({
+    validationSchema: {
+        firstname(value: string) {
+            if (value?.length >= 2) return true
+
+            return 'Jméno musí mít alespoň 2 znaky.'
+        },
+        lastname(value: string) {
+            if (value?.length >= 2) return true
+
+            return 'Příjmení musí mít alespoň 2 znaky.'
+        },
+        email(value: string) {
+            if (/^[a-z.-]+@[a-z.-]+\.[a-z]+$/i.test(value)) return true
+
+            return 'Neplatná e-mailová adresa.'
+        },
+        checkbox(value: any) {
+            if (value === '1') return true
+
+            return 'Musí být potvrzeno.'
+        },
+    },
+});
+
+const firstname = useField('firstname');
+const lastname = useField('lastname');
+let email = useField('email');
+const checkbox = useField('checkbox');
+
+// retrieve store
+const store = useUserStore()
+
+const submit = handleSubmit(userData => {
+    store.createUser(userData.firstname, userData.lastname, userData.email)
+    useRouter().push({name:'form'})
+});
+
+</script>
+
+<style scoped>
+:deep(.v-messages__message) {
+    margin-bottom: 20px;
+}
+
+.rounded-corner {
+    border-top-left-radius: 10%;
+}
+
+</style>
