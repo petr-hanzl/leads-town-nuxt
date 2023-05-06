6b0eaf4ebe2e5605be15a5411cfbf8647a414ef9
Dita Němečková <ditusn@gmail.com>
2023-05-06 22:14:02 +0200

    create chart

---
+++ components/newVersion/Chart.vue
@@ -0,0 +1,41 @@
+<template>
+    <v-container class="fill-height">
+        <v-row class="h-50" justify="center">
+            <Bar :data="data" />
+        </v-row>
+    </v-container>
+</template>
+
+<script setup lang="ts">
+import { Bar } from 'vue-chartjs'
+import { useTheme } from "vuetify";
+import { Chart, registerables } from 'chart.js';
+Chart.register(...registerables)
+
+const data = {
+  labels: [
+    'PPC',
+    'Sociální sítě',
+    'Emailing',
+    'Label',
+    'Label',
+  ],
+  datasets: [
+    {
+        label: 'Data',
+        backgroundColor:
+            [
+                useTheme().current.value.colors.primary,
+                useTheme().current.value.colors.error,
+                useTheme().current.value.colors.secondary,
+            ],
+        data: [12, 39, 10, 70, 39]
+    }
+  ]
+}
+
+</script>
+
+<style scoped>
+
+</style>
