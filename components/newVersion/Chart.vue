74e90231429ac4e397c344d0a07e6bcc663764b3
Petr Hanzl <petrhanzl33@gmail.com>
2023-05-08 17:48:39 +0200

    Add small style to chart

--- components/newVersion/Chart.vue
+++ components/newVersion/Chart.vue
@@ -1,7 +1,32 @@
-<template>
-    <v-container class="fill-height">
-        <v-row class="h-50" justify="center">
-            <Bar :data="data" />
+<template xmlns="http://www.w3.org/1999/html">
+    <v-container fluid class="fill-height h-screen d-flex flex-column justify-space-between pa-6">
+        <v-row class="w-100">
+            <v-col cols="6" class="d-flex justify-end">
+                <nuxt-img src="/logo.png"  alt="leads town"/>
+            </v-col>
+        </v-row>
+
+        <v-row class="w-100 ma-0 text-primary" align="center" justify="center">
+            <v-col cols="12" md="8" lg="6" class="d-flex flex-column align-center">
+                <!-- question -->
+                <v-col cols="12">
+                    <p class="text-h4 text-sm-h3 text-center font-weight-black">
+                        Výsledky:
+                    </p>
+                </v-col>
+                <v-container class="fill-height">
+                    <v-row class="h-50" justify="center">
+                        <Bar :data="data" />
+                    </v-row>
+                </v-container>
+            </v-col>
+        </v-row>
+
+        <v-row class="w-100 text-primary-lighten-2  text-h6">
+            <!-- contact -->
+            <v-col cols="6" class="d-flex justify-start align-end">
+                <p>hi@lead.town</p>
+            </v-col>
         </v-row>
     </v-container>
 </template>
@@ -13,6 +38,7 @@
     import { Chart, registerables } from 'chart.js';
     Chart.register(...registerables)
 
+
     // todo use this when answerStore works
     const l = new Array<string>
     const d = new Array<number>
@@ -20,6 +46,7 @@
         l.push(category)
         d.push(result)
     })
+
 
 
     const data = {
